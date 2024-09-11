f47bab18d1f59cad4ba56e605dd765a489ae1cef
victor <vitormachado2001@gmail.com>
2024-09-11 20:38:52 -0300

    Teoricamente integrado com o Banco de dados, só tem que testar agora essa bomba.

--- ex1-biblioteca-v2-dadosMemoria/src/main/java/com/bcopstein/ex1biblioeca/Record.java
+++ ex1-biblioteca-v2-dadosMemoria/src/main/java/com/bcopstein/ex1biblioeca/Record.java
@@ -1,23 +1,22 @@
 package com.bcopstein.ex1biblioeca;
 
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.sql.JDBCType;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Record {
-    private List<Livro> livros;
-
-    public Record(Jd) {
-        livros = new LinkedList<>();
-        livros.add(new Livro(100, "Aprendendo Spring-Boot", "Huguinho Pato", 2020));
-        livros.add(new Livro(120, "Aprendendo Java", "Zezinho Pato", 2015));
-        livros.add(new Livro(140, "Aprendendo Outra coisa", "Luizinho Pato", 2023));
-        livros.add(new Livro(140, "Aprendendo Uma coisa nova", "Huguinho Pato", 2023));
-        livros.add(new Livro(140, "Aprendendo Outra coisa nova", "Huguinho Pato", 2023));
+private JdbcTemplate jdbcTemplate;
+    public Record(JdbcTemplate jdbcTemplate1) {
+        this.jdbcTemplate = jdbcTemplate1;
     }
 
     public List<Livro> getLivros() {
-        return livros;
+        List<Livro> livros = jdbcTemplate.
+
     }
 
     public boolean add(Livro livro){return livros.add(livro);}
